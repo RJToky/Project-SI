7916b4dade990d71e324fdc6774aa6ef389dbfa5
RJToky <tokyrakotoarivony5@gmail.com>
2023-07-08 19:15:54 +0300

    Init Project

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/Project-SI/';
+//$config['base_url'] = 'https://rjtoky.alwaysdata.net/Demo/';
 
 /*
 |--------------------------------------------------------------------------
@@ -385,9 +386,8 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = APPPATH . "cache_session/";
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
