f557b5f87f1b92509b24e84b8aab65ca28b698d3
Miali1865 <mialivolarandrianaivo@gmail.com>
2023-07-10 10:21:03 +0100

    Tokony mety

--- sql/data.sql
+++ sql/data.sql
@@ -2,7 +2,8 @@ insert into users (nomUser,prenomUser,genreUser,naissanceUser,mailUser,mdpUser) 
 ('FitDreamer','Sarah',0,'1990-07-12','sarah90fit@gmail.com',md5('sarah13')),
 ('ActiveLion','David',1,'1985-05-25','david85active@gmail.com',md5('david85')),
 ('HealthyJourney','Emily',1,'1992-10-08','emily92healthy@gmail.com',md5('emily92')),
-('FitExplorer','Alex',1,'1988-10-08','emily92healthy@gmail.com',md5('emily92')),
+('FitExplorer','Alex',1,'1988-06-17','emily92healthy@gmail.com',md5('alex88')),
+('WellnessSeeker','Laura',1,'1994-11-05','laura94wellness@gmail.com',md5('laura94'));
 
 
 insert into detailUser(idUser,tailleUser,poidsUser,dateUpdateDetailUser) values (1,160,48,'2023-07-10');
