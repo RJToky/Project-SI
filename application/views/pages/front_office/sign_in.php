1a734c675b5247a88f1843eb879d727febf75407
RJToky <tokyrakotoarivony5@gmail.com>
2023-07-10 15:34:57 +0300

    [MERGE] Project

--- application/views/pages/front_office/sign_in.php
+++ application/views/pages/front_office/sign_in.php
@@ -24,11 +24,11 @@
 		<form id="form-login" class="flex flex-col w-1/2" action="#" method="post">
 			<div class="p-4 flex flex-col gap-2">
 				<label class="text-gray-500 text-lg font-semibold">Entrer votre email:</label>
-				<input class="w-full text-lg h-14 px-6 py-2 rounded-full bg-[#F6F6F6] focus:outline-none text-gray-500" type="email" placeholder="example@example.com">
+				<input name="mail" class="w-full text-lg h-14 px-6 py-2 rounded-full bg-[#F6F6F6] focus:outline-none text-gray-500" type="email" placeholder="example@example.com">
 			</div>
 			<div class="p-4">
 				<label class="text-gray-500 text-lg font-semibold">Entrer votre mot de passe:</label>
-				<input class="w-full text-lg h-14 px-6 py-2 rounded-full bg-[#F6F6F6] focus:outline-none text-gray-500" type="password" placeholder="Mot de passe">
+				<input name="mdp" class="w-full text-lg h-14 px-6 py-2 rounded-full bg-[#F6F6F6] focus:outline-none text-gray-500" type="password" placeholder="Mot de passe">
 			</div>
 			<div class="p-4">
 				<button id="btn-submit" type="submit" class="hover:bg-[#2e8c9b] w-full h-14 px-4 py-2 rounded-full bg-[#39AEC0] text-center font-semibold text-white text-lg">
@@ -41,15 +41,15 @@
 		</form>
 	</div>
 
-	<div id="toast-danger" class="fixed top-4 right-4 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+	<div id="toast-danger" class="hidden fixed top-4 right-4 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
     <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
         </svg>
         <span class="sr-only">Error icon</span>
     </div>
-    <div class="ml-3 text-sm font-normal">Item has been deleted.</div>
-    <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+    <div id="message-error" class="ml-3 text-sm font-normal">Item has been deleted.</div>
+    <button id="button-close" type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
         <span class="sr-only">Close</span>
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -59,24 +59,34 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 	<script>
 		$(document).ready(() => {
-			$("#btn-submit").click((e) => {
+			$("#form-login").submit((e) => {
+				e.preventDefault();
 				$("#btn-submit span:nth-child(1)").removeClass("hidden");
 				$("#btn-submit span:nth-child(2)").addClass("hidden");
+				var formData = new FormData(document.getElementById("form-login"));
 
-				e.preventDefault();
 				$.ajax({
 					method: "POST",
-					url: "<?= base_url("Welcome/login") ?>",
+					url: "<?= base_url("C_User/login") ?>",
+					data: formData,
+					processData: false,
+					contentType: false,
 					success: (response) => {
-						console.log(response);
+						let res = JSON.parse(response);
+						if(res.response === "success") {
+							window.location.href = "<?= base_url("C_Home") ?>";
+						} else if (res.response === "error") {
+							$("#message-error").text(res.message);
+							$("#toast-danger").removeClass("hidden");
+						}
+						$("#btn-submit span:nth-child(1)").addClass("hidden");
+						$("#btn-submit span:nth-child(2)").removeClass("hidden");
 					},
-					complete: () => {
-						setTimeout(() => {
-							$("#btn-submit span:nth-child(1)").addClass("hidden");
-							$("#btn-submit span:nth-child(2)").removeClass("hidden");
-						}, 1000);
-					}
 				});
+			});
+
+			$("#button-close").click(() => {
+				$("#toast-danger").addClass("hidden");
 			});
 		});
 	</script>
