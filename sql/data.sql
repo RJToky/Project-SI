d8e8ddaf28803b9dd6953f5d0ed8896ee30ba619
vanioh <berjayav@gmail.com>
2023-07-10 09:42:39 +0100

    Base users

---
+++ sql/data.sql
@@ -0,0 +1,3 @@
+insert into users (nomUser,prenomUser,genreUser,naissanceUser,mailUser,mdpUser) values ('Randrianaivo','Mialivola',0,'2004-01-16','mialivolarandrianaivo@gmail.com','mialivola');
+
+insert into detailUser(idUser,tailleUser,poidsUser,dateUpdateDetailUser) values (1,160,48,'2023-07-10');
---
+++ sql/table.sql
@@ -0,0 +1,23 @@
+create database regime;
+
+\c regime;
+
+create table users (
+    idUser serial primary key,
+    nomUser varchar,
+    prenomUser varchar,
+    genreUser int,
+    naissanceUser date,
+    mailUser varchar,
+    mdpUser varchar
+);
+
+create table detailUser (
+    idDetailUser serial primary key,
+    idUser int,
+    tailleUser float,
+    poidsUser float,
+    dateUpdateDetailUser date,
+    foreign key (idUser) references users (idUser)
+);
+
