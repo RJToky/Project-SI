5f907cac17e7689dc2d89aff8024390bcc6a40a6
RJToky <tokyrakotoarivony5@gmail.com>
2023-07-11 16:51:15 +0300

    [ADD] Front Validation code

--- application/views/pages/back_office/code.php
+++ application/views/pages/back_office/code.php
@@ -16,6 +16,47 @@
 
     <?php $this->load->view("partials/back_office/header"); ?>
 
+    <div class="shadow-md rounded-md bg-white p-6 w-full mt-4">
+      <div class="relative overflow-x-auto mb-10">
+          <h2 class="mb-3 text-xl font-medium text-[#39AEC0]">Validation des codes</h2>
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" class="px-6 py-3">
+                      Nom d'utilisateur
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                      Code
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                      Montant
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                      Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        1
+                    </th>
+                    <td class="px-6 py-4">
+                        Coucou
+                    </td>
+                    <td class="px-6 py-4">
+                        Hello
+                    </td>
+                    <td class="px-6 py-4">
+                      <a href="<?= base_url("C_Admin/valider/") ?>" id="btn-update" class="hover:bg-[#40c4d8] bg-[#39AEC0] transition-all duration-300 px-4 py-2 text-center font-semibold text-white focus:outline-none">
+                        Valider
+                      </a>
+                    </td>
+                </tr>
+              </tbody>
+          </table>
+      </div>
+    </div>
     
   </main>
 
--- application/views/partials/back_office/nav.php
+++ application/views/partials/back_office/nav.php
@@ -2,9 +2,9 @@
   <a href="#" class="text-[#39AEC0] text-4xl font-bold text-center" style="font-family: 'Birica';">Asio'Rezim</a>
   <nav class="flex flex-col">
     <a href="<?= base_url("C_Admin/index") ?>" class="pl-6 font-normal py-3 text-lg text-gray-600 hover:bg-gray-100">Accueil</a>
+    <a href="<?= base_url("C_Admin/code") ?>" class="pl-6 font-normal py-3 text-lg text-gray-600 hover:bg-gray-100">Code porte monnaie</a>
     <a href="<?= base_url("C_Admin/regime") ?>" class="pl-6 font-normal py-3 text-lg text-gray-600 hover:bg-gray-100">Régime</a>
     <a href="<?= base_url("C_Admin/activite_sportive") ?>" class="pl-6 font-normal py-3 text-lg text-gray-600 hover:bg-gray-100">Activité sportive</a>
-    <a href="<?= base_url("C_Admin/code") ?>" class="pl-6 font-normal py-3 text-lg text-gray-600 hover:bg-gray-100">Code porte monnaie</a>
   </nav>
 </div>
 
